7f00407d8107ea5b1ba642ec59cc11de872e8a03
DanielDruetta <danidruetta_97@hotmail.com>
2019-03-15 16:33:08 -0300

    Update ejercicio-02.py

--- practico_01/ejercicio-02.py
+++ practico_01/ejercicio-02.py
@@ -1,5 +1,19 @@
 # Implementar la función mayor, que reciba tres números y devuelva el mayor de ellos.
 
 
-def mayor(a, b, c):
-    pass
+
+
+def mayor(a,b,c):
+    
+    if a > b and a > c:
+        return a
+
+    if b > c and b > a:
+        return b
+
+    if c > b and c > a:
+        return c
+
+assert mayor(2, 5, 7) == 7
+
+
