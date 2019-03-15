881eca05dc34fe5f0a127a278e23eeb3270dbc72
DanielDruetta <danidruetta_97@hotmail.com>
2019-03-15 16:09:32 -0300

    Update ejercicio-01.py

--- practico_01/ejercicio-01.py
+++ practico_01/ejercicio-01.py
@@ -1,5 +1,14 @@
 # 1. Implementar una función max() que tome como argumento dos números y devuelva el mayor de ellos. 
 
+a=5
+b=10
 
-def maximo(a, b):
-    pass
+def maximo (a,b):
+    if a >= b:
+        return a
+
+    return b
+
+
+assert maximo (10,5)==10
+assert maximo (9,18)==18
