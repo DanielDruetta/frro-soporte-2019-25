4027332fafd0d6853b520839da3d10460b0d7fb8
AgustinYurescia <48600998+AgustinYurescia@users.noreply.github.com>
2019-03-19 17:33:02 -0300

    Update ejercicio-04.py

--- practico_01/ejercicio-04.py
+++ practico_01/ejercicio-04.py
@@ -3,4 +3,10 @@
 
 
 def conversor(grados):
-    pass
+    far=((grados*9/5)+32)
+    return far
+assert conversor(1) == 33.8
+
+#Para pedir el ingreso del dato por teclado y devolver el valor en Farenheit:
+#grados = float(input('Introduzca una temperatura en grados celsius: '))
+#print('\nLa temperatura en Farenheit es: ',conversor(grados))
