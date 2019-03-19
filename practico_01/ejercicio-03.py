86a4f72f062cb3d7ce59c3806cd8b14beb2000de
AgustinYurescia <48600998+AgustinYurescia@users.noreply.github.com>
2019-03-19 17:24:33 -0300

    Update ejercicio-03.py

--- practico_01/ejercicio-03.py
+++ practico_01/ejercicio-03.py
@@ -4,5 +4,14 @@
 # - Si multiplicar es False y b es cero: imprimir por consola "Operación no valida".
 
 
-def operacion(a, b, multiplicar):
-    pass
+def operacion(a,b,multiplicar):
+    if multiplicar:
+        return (a*b)
+    elif (b==0):
+        return 'Operacion no valida'
+    else:
+        return (a/b)
+
+assert operacion(8,2,True) ==  16
+assert operacion(8,2,False) ==  4
+assert operacion(8,0,False) == 'Operacion no valida'
